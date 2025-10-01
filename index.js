9cfcbbc981ba017549e58d7fcb36b0eb6d8190a0
rohitshinde1020 <rs5559420@gmail.com>
2025-10-01 14:37:09 +0530

    new repo with first game

---
+++ index.js
@@ -0,0 +1,54 @@
+let number = Math.floor(Math.random() * 100) + 1;
+
+let prevguess = document.getElementById('prevguess');
+let currans = document.getElementById('currans');
+let hiorlow = document.getElementById('hiorlow');
+
+let submitguess = document.getElementById('submitguess');
+let userinput = document.getElementById('userinput');
+
+let guesscount = 1;
+let resetbutton;
+
+submitguess.addEventListener('click',body);
+
+function body() {
+    const userguess = Number(userinput.value);
+
+    if (guesscount === 1) {
+        prevguess.textContent = "Previous guesses:";
+    }
+    prevguess.textContent += " " + userguess;
+
+    if (userguess===number) {
+        currans.textContent="✅ Right answer! You got it correct";
+        currans.style.backgroundColor="green";
+        hiorlow.textContent='';
+        setGameOver();
+    }
+
+    else if (guesscount === 10) {
+        currans.textContent="❌ Game over";
+        hiorlow.textContent="";
+        prevguess.textContent=" "
+        setGameOver();
+    } 
+    
+    else {
+        currans.textContent ="Wrong!";
+        currans.style.backgroundColor = "red";
+
+        if (userguess > number) {
+            hiorlow.textContent ="The number is too high";
+        } 
+
+        else {
+            hiorlow.textContent ="The number is too low";
+        }
+    }
+
+    guesscount++;
+    userinput.value = "";
+    userinput.focus();
+}
+
